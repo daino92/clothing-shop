f51f4873816433de5dc53154c9a14b658c82db11
Dionysis Kalepanagos <daino92@gmail.com>
2019-10-27 22:02:43 +0200

    Redux saga react hooks (#18)
    * intregrated stripe to project (#12) (#13)
    
    * added sagas to the project. converted fetchCollectionsStart in a saga way
    
    * converting email and google sign-in to saga. Also made user persistence
    
    * converted sign out functionality to saga
    
    * clear cart upon sign-out and sign-up and auto sign-in after sign-up with sagas
    
    * use hooks useEffect on sign-up and sign-in components
    
    * use hooks useEffect on App.js and shop component

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,77 +1,58 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import {connect} from 'react-redux';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 import {TitleComponent, SignUpComponent} from './sign-up.styles';
-import {auth, createUserProfileDocument} from '../../firebase/firebase.utils';
+import {signUpStart} from '../../redux/user/user.actions';
 
-class SignUp extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-            displayName: '',
-            email: '',
-            password: '',
-            confirmPassword: ''
-        }
-    }
+const SignUp = ({signUpStart}) => {
+    const [userCredentials, setUserCredentials] = useState({
+        displayName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
     
-    handleSubmit = async event => {
+    const {displayName, email, password, confirmPassword} = userCredentials;
+
+    const handleSubmit = async event => {
         event.preventDefault();
 
-        const {displayName, email, password, confirmPassword} = this.state;
-        
         if (password !== confirmPassword) {
             alert("passwords don't match");
             return;
         }
-
-        try {
-            const {user} = await auth.createUserWithEmailAndPassword(
-                email, 
-                password
-            );
-
-            await createUserProfileDocument(user, {displayName});
-
-            this.setState({
-                displayName: '',
-                email: '',
-                password: '',
-                confirmPassword: ''
-            })
-        } catch (error) {
-            console.error(error)
-        }
+        
+        signUpStart({displayName, email, password})
     }
 
-    handleChange = event => {
+    const handleChange = event => {
         const {name, value} = event.target;
 
-        this.setState({ [name]: value })
+        setUserCredentials({...userCredentials, [name]: value })
     }
 
-    render() {
-        const {displayName, email, password, confirmPassword} = this.state;
-        return (
-            <SignUpComponent>
-                <TitleComponent as='h2'>I do not have an account</TitleComponent>
-                <span>Sign up with your email and password</span>
-                <form className='sign-up-form' onSubmit={this.handleSubmit}>
-                    <FormInput type='text' name='displayName' label='Display Name'
-                        value={displayName} onChange={this.handleChange} required/>
-                    <FormInput type='email' name='email' label='Email'
-                        value={email} onChange={this.handleChange} required/>
-                    <FormInput type='password' name='password' label='Password'
-                        value={password} onChange={this.handleChange} required/>
-                    <FormInput type='password' name='confirmPassword' label='Confirm Password'
-                        value={confirmPassword} onChange={this.handleChange} required/>
-                    <CustomButton type='submit'>SIGN UP</CustomButton>
-                </form>
-            </SignUpComponent>    
-        )
-    }
+    return (
+        <SignUpComponent>
+            <TitleComponent as='h2'>I do not have an account</TitleComponent>
+            <span>Sign up with your email and password</span>
+            <form className='sign-up-form' onSubmit={handleSubmit}>
+                <FormInput type='text' name='displayName' label='Display Name'
+                    value={displayName} onChange={handleChange} required/>
+                <FormInput type='email' name='email' label='Email'
+                    value={email} onChange={handleChange} required/>
+                <FormInput type='password' name='password' label='Password'
+                    value={password} onChange={handleChange} required/>
+                <FormInput type='password' name='confirmPassword' label='Confirm Password'
+                    value={confirmPassword} onChange={handleChange} required/>
+                <CustomButton type='submit'>SIGN UP</CustomButton>
+            </form>
+        </SignUpComponent>    
+    )
 }
 
-export default SignUp;
+const mapDispatchToProps = dispatch => ({
+    signUpStart: userCredentials => dispatch(signUpStart(userCredentials))
+})
+
+export default connect(null, mapDispatchToProps)(SignUp);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Route} from 'react-router-dom';
 import {connect} from 'react-redux';
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
 import CollectionsPageContainer from '../collection/collection.container';
-import {fetchCollectionsStartAsync} from '../../redux/shop/shop.actions';
+import {fetchCollectionsStart} from '../../redux/shop/shop.actions';
 
-class ShopPage extends React.Component {
-    componentDidMount() {
-        const {fetchCollectionsStartAsync} = this.props;
-        fetchCollectionsStartAsync();
-    }
+const ShopPage = ({fetchCollectionsStart, match}) => {
+    useEffect(() => {
+        fetchCollectionsStart()
+    }, [fetchCollectionsStart])
 
-    render() {
-        const {match} = this.props;
-        return (
-            <div className='shop-page'>
-                <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
-                <Route path={`${match.path}/:collectionId`} component={CollectionsPageContainer} />
-            </div>
-        )
-    }
+    return (
+        <div className='shop-page'>
+            <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
+            <Route path={`${match.path}/:collectionId`} component={CollectionsPageContainer} />
+        </div>
+    )
 }
 
 const mapDispatchToProps = dispatch => ({
-    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+    fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 })
 
 export default connect(null, mapDispatchToProps)(ShopPage);
